b2a8802c29f59da2f3b463bd4238cd1b6fa5e225
João Palmeiro <joaommpalmeiro@gmail.com>
2022-12-01 20:02:36 +0000

    Add boilerplate

---
+++ gift-search-bar/README.md
@@ -0,0 +1,15 @@
+# Advent Of Vue 2022: Gift Search Bar
+
+## Development
+
+- `npm install`
+- `npm run dev`
+
+## References
+
+- https://github.com/Advent-Of-Vue/2022-gift-search-bar
+- https://github.com/Advent-Of-Vue/2022-gift-search-bar/tree/solution
+
+## Notes
+
+- https://docs.npmjs.com/creating-a-package-json-file#required-name-and-version-fields
---
+++ gift-search-bar/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      // https://www.npmjs.com/package/@vitejs/plugin-vue
+      // https://vuejs.org/guide/extras/reactivity-transform.html
+      reactivityTransform: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+  },
+})
